9b879201dfd4ea1df130b08a79209eb06b00ac37
bryan-lenovo <bryanchlam@outlook.com>
2022-03-03 21:39:05 +0800

    Add code snippet 1

--- CH2.java
+++ CH2.java
@@ -1,1 +1,11 @@
-Hello World.
+public class CH2 {
+    public CH2() {
+        number = 5;
+    }
+    public static void main(String[] args) {
+        CH2 ch2 = new CH2();
+        System.out.println(ch2.number);
+    }
+    private int number = 3;
+    { number = 4;}
+}
